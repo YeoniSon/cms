05e0cd5b074926f3e8cec8ac9b199ea3e628b370
YeoniSon <sjy8211@gmail.com>
2025-07-31 14:45:06 +0900

    feat : [004-LOGIN-CUSTOMER] 커스터머에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/* 에 대한 token filter 적용

--- user-api/src/main/java/com/example/cms/user/UserApplication.java
+++ user-api/src/main/java/com/example/cms/user/UserApplication.java
@@ -3,14 +3,18 @@ package com.example.cms.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
+@ServletComponentScan
 @EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
+@ComponentScan(basePackages = {"com.example.cms.user", "com.zerobase.domain"})
 @RequiredArgsConstructor
 public class UserApplication {
 
---
+++ user-api/src/main/java/com/example/cms/user/application/SignInApplication.java
@@ -0,0 +1,32 @@
+package com.example.cms.user.application;
+
+import com.example.cms.user.domain.SignInForm;
+import com.example.cms.user.domain.model.Customer;
+import com.example.cms.user.exception.CustomException;
+import com.example.cms.user.service.CustomerService;
+
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import com.zerobase.domain.domain.common.UserType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.example.cms.user.exception.ErrorCode.LOGIN_CHECK_FAIL;
+
+@Service
+@RequiredArgsConstructor
+public class SignInApplication {
+
+    private final CustomerService customerService;
+    private final JwtAuthenticationProvider provider;
+
+    public String customerLoginToken(SignInForm form) {
+        // 1. 로그인 가능 여부
+        Customer c = customerService.findValidCustomer(form.getEmail(), form.getPassword())
+                .orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
+
+        // 2. 토큰을 발행하고
+
+        // 3. 토큰을 response한다.
+        return provider.createToken(c.getEmail(), c.getId(), UserType.CUSTOMER);
+    }
+}
