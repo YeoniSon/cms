b3956db46c9ed54db3585456162f8b613638f7e7
YeoniSon <sjy8211@gmail.com>
2025-08-01 18:36:06 +0900

    feat : [010-PRODUCT_MODIFY] Product & ProductItem Modify
    Product & ProductItem Modify

--- order-api/src/main/java/com/example/cms/order/controller/SellerProductController.java
+++ order-api/src/main/java/com/example/cms/order/controller/SellerProductController.java
@@ -1,10 +1,6 @@
-package com.example.cms.order;
+package com.example.cms.order.controller;
 
-import com.example.cms.order.domain.model.Product;
-import com.example.cms.order.domain.product.AddProductForm;
-import com.example.cms.order.domain.product.AddProductItemForm;
-import com.example.cms.order.domain.product.ProductDto;
-import com.example.cms.order.domain.product.ProductItemDto;
+import com.example.cms.order.domain.product.*;
 import com.example.cms.order.service.ProductItemService;
 import com.example.cms.order.service.ProductService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
@@ -33,13 +29,37 @@ public class SellerProductController {
         );
     }
 
-    @PostMapping("/item")
+    @PutMapping("/item")
     public ResponseEntity<ProductDto> addProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token,
                                                      @RequestBody AddProductItemForm form) {
 
 
         return ResponseEntity.ok(ProductDto.from(
                 productItemService.addProductItem(
+                                provider.getUserVo(token).getId(), form)
+                )
+        );
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductDto> updateProduct(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                 @RequestBody UpdateProductForm form) {
+
+
+        return ResponseEntity.ok(ProductDto.from(
+                        productService.updateProduct(
+                                provider.getUserVo(token).getId(), form)
+                )
+        );
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<ProductItemDto> updateProductItem(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                     @RequestBody UpdateProductItemForm form) {
+
+
+        return ResponseEntity.ok(ProductItemDto.from(
+                        productItemService.updateProductItem(
                                 provider.getUserVo(token).getId(), form)
                 )
         );
--- order-api/src/main/java/com/example/cms/order/service/ProductService.java
+++ order-api/src/main/java/com/example/cms/order/service/ProductService.java
@@ -1,11 +1,19 @@
 package com.example.cms.order.service;
 
 import com.example.cms.order.domain.model.Product;
+import com.example.cms.order.domain.model.ProductItem;
 import com.example.cms.order.domain.product.AddProductForm;
+import com.example.cms.order.domain.product.UpdateProductForm;
+import com.example.cms.order.domain.product.UpdateProductItemForm;
 import com.example.cms.order.domain.repository.ProductRepository;
+import com.example.cms.order.exception.CustomException;
+import com.example.cms.order.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.cms.order.exception.ErrorCode.NOT_FOUND_ITEM;
+import static com.example.cms.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +23,23 @@ public class ProductService {
     @Transactional
     public Product addProduct(Long sellerId, AddProductForm form) {
         return productRepository.save(Product.of(sellerId, form));
+    }
+
+    public Product updateProduct(Long sellerId, UpdateProductForm form) {
+        Product product = productRepository.findBySellerIdAndId(sellerId, form.getId())
+                .orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
+        product.setDescription(form.getDescription());
+        product.setName(form.getName());
+
+        for (UpdateProductItemForm itemForm : form.getItems()) {
+            ProductItem item = (ProductItem) product.getProductItems().stream()
+                    .filter(pi -> pi.getId().equals(itemForm.getId()))
+                    .findFirst().orElseThrow(() -> new CustomException(NOT_FOUND_ITEM));
+
+            item.setName(form.getName());
+            item.setPrice(itemForm.getPrice());
+            item.setCount(itemForm.getCount());
+        }
+        return product;
     }
 }
