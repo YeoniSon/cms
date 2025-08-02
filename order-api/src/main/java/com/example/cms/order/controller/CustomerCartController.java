a85b9d63ea8900f7314715c14690bc25280048ab
YeoniSon <sjy8211@gmail.com>
2025-08-02 13:06:38 +0900

    feat : [015-UPDATE_CART 카트 업데이트 - 카트 조회 로직을 재사용하여 코드 생산성을 높였음

--- order-api/src/main/java/com/example/cms/order/controller/CustomerCartController.java
+++ order-api/src/main/java/com/example/cms/order/controller/CustomerCartController.java
@@ -29,9 +29,19 @@ public class CustomerCartController {
     @GetMapping
     public ResponseEntity<Cart> showCart(
             @RequestHeader(name = "X-AUTH-TOKEN") String token
-    ){
+    ) {
         return ResponseEntity.ok(
                 cartApplication.getCart(provider.getUserVo(token).getId())
+        );
+    }
+
+    @PutMapping
+    public ResponseEntity<Cart> updateCart(
+            @RequestHeader(name = "X-AUTH-TOKEN") String token,
+            @RequestBody Cart cart
+    ) {
+        return ResponseEntity.ok(
+                cartApplication.updateCart(provider.getUserVo(token).getId(), cart)
         );
     }
 }
